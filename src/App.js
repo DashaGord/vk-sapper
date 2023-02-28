677bf4c2a5ad427138e871eb81041d6af8e1304a
Gordienko Daria <gordienko.dasha45@gmail.com>
2023-02-28 23:09:14 +0300

    The logic for rendering grid-container has been implemented.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  const rows = [];
+  for (let i = 0; i < 16; i++) {
+    const cols = [];
+    for (let j = 0; j < 16; j++) {
+      cols.push(<Col key={j} className={`grid-col f-${j}-${i}`} />);
+    }
+    rows.push(<div key={i} className="grid-row"><Row>{cols}</Row></div>);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id='global'>
+
+        <Container fluid className="grid-container">
+          {rows}
+        </Container>
+
+      </div>
   );
 }
 
